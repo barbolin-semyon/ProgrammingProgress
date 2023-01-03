161b5781aa786af653371fdcf4225da74fb92b9e
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-03 14:44:53 +0300

    set colors

--- app/src/main/java/com/example/programmingprogress/ui/theme/Color.kt
+++ app/src/main/java/com/example/programmingprogress/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.example.programmingprogress.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Orange = Color(0xFFFFBB73)
+val White = Color(0xFFFFFFFF)
+val Gray = Color(0xFFFFFDFD)
+val Red = Color(0xFFFF7373)
+val Blue = Color(0xFF007EA7)
+val DarkBlue = Color(0xFF00526D)
