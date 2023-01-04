5e8210eb042667f2c6243da1ed4d82b9223d18f7
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-04 20:01:24 +0300

    Добавление цвета DarkColor

--- app/src/main/java/com/example/programmingprogress/ui/theme/Color.kt
+++ app/src/main/java/com/example/programmingprogress/ui/theme/Color.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Orange = Color(0xFFFFBB73)
 val White = Color(0xFFFFFFFF)
 val Gray = Color(0xFFFFFDFD)
+val DarkGray = Color(0xFF494949)
 val Red = Color(0xFFFF7373)
 val Blue = Color(0xFF007EA7)
 val DarkBlue = Color(0xFF00526D)
