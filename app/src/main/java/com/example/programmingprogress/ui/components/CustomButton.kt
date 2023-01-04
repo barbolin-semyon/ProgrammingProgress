dd2e11de4c97356fb8d169b7446d82f4c619e97b
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-04 20:02:38 +0300

    Имзенение CustomButtonFillSize

--- app/src/main/java/com/example/programmingprogress/ui/components/CustomButton.kt
+++ app/src/main/java/com/example/programmingprogress/ui/components/CustomButton.kt
@@ -9,12 +9,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.programmingprogress.ui.theme.ProgrammingProgressTheme
 import com.example.programmingprogress.ui.theme.White
 
 @Composable
-private fun CustomButtonFillSize(color: Color, text: String, onClick: () -> Unit) {
+fun CustomButtonFillSize(color: Color, text: String, topPadding: Dp = 0.dp, onClick: () -> Unit) {
     Button(
         onClick = { onClick() },
         colors = ButtonDefaults.buttonColors(
@@ -22,7 +23,7 @@ private fun CustomButtonFillSize(color: Color, text: String, onClick: () -> Unit
         ),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .padding(top = topPadding, start = 8.dp, end = 8.dp)
     ) {
         Text(text = text, color = White)
     }
