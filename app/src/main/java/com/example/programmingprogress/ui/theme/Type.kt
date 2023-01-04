f8a8f308c159af4fa736df49b35f82d6c1771da3
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-04 17:52:57 +0300

    Добавление в типографию стилей

--- app/src/main/java/com/example/programmingprogress/ui/theme/Type.kt
+++ app/src/main/java/com/example/programmingprogress/ui/theme/Type.kt
@@ -12,8 +12,14 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
+
+    h5 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+
     button = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
@@ -24,5 +30,4 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     )
-    */
 )
