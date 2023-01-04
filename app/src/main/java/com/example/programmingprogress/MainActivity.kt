7b971e6993631824d4030ba8172a7ef883cd5a45
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-04 16:42:54 +0300

    Установление NavHost и BottomNavigation в MainActivity.kt

--- app/src/main/java/com/example/programmingprogress/MainActivity.kt
+++ app/src/main/java/com/example/programmingprogress/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.programmingprogress
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.programmingprogress.ui.navigation.AppBottomNavigation
+import com.example.programmingprogress.ui.navigation.AppNavHost
+import com.example.programmingprogress.ui.navigation.Screen
+import com.example.programmingprogress.ui.theme.Orange
 import com.example.programmingprogress.ui.theme.ProgrammingProgressTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +27,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProgrammingProgressTheme {
 
+                val UIController = rememberSystemUiController()
+                UIController.setStatusBarColor(Orange)
+
+                val navController = rememberNavController()
+                val startDestination = Screen.Main.route
+
+                Scaffold(bottomBar = {
+                    AppBottomNavigation(
+                        navHostController = navController,
+                    )
+                }) {
+                    Box(modifier = Modifier.padding(it)) {
+                        AppNavHost(
+                            navController = navController,
+                            startDestination = startDestination
+                        )
+                    }
+                }
             }
         }
     }
