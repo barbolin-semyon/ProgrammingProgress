5d98a074dba7a80707ddd9a26e06b293f0890dda
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-04 14:27:24 +0300

    Реализация NavHost и исправление бага в Screens.kt

--- app/src/main/java/com/example/programmingprogress/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/programmingprogress/ui/navigation/Screens.kt
@@ -2,23 +2,23 @@ package com.example.programmingprogress.ui.navigation
 
 import com.example.programmingprogress.R
 
-sealed class Screen(route: String) {
+sealed class Screen(val route: String) {
     object Authorization : Screen("authorization")
     object Main: Screen("main")
 }
 
-sealed class AuthorizationScreen(route: String) {
+sealed class AuthorizationScreen(val route: String) {
     object SignInScreen : AuthorizationScreen("signIn")
     object RegistrationScreen : AuthorizationScreen("registration")
 }
 
-sealed class MainScreen(route: String, icon: Int) {
+sealed class MainScreen(val route: String, val icon: Int) {
     object Checkpoints : MainScreen("checkpoint", R.drawable.checkpoint)
     object Diagram : MainScreen("diagram", R.drawable.diagram)
     object History : MainScreen("history", R.drawable.profile)
 }
 
-sealed class HistoryScreen(route: String) {
+sealed class HistoryScreen(val route: String) {
     object ListHistoryScreen: HistoryScreen("listHistory")
     object DetailHistoryScreen: HistoryScreen("detailHistory")
 }
