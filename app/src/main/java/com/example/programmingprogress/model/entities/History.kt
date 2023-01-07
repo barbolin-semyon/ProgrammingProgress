f5144a4f4b3914de5a0ffaa3d33b016340fd7e5c
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-07 23:14:13 +0300

    Добавление конструктора

--- app/src/main/java/com/example/programmingprogress/model/entities/History.kt
+++ app/src/main/java/com/example/programmingprogress/model/entities/History.kt
@@ -1,10 +1,11 @@
 package com.example.programmingprogress.model.entities
 
+import java.sql.Timestamp
 import java.util.Date
 
 data class History(
-    val date: Date,
-    val description: String,
-    val isCheck: Boolean,
-    val hours: Double,
+    val date: Date = Date(),
+    val description: String = "",
+    val isCheck: Boolean = false,
+    val hours: Double = 0.0,
 )
