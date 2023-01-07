e29269a0d1af283afbf0c7e3565287115eb5d776
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-07 23:14:26 +0300

    Возвращение функции

--- app/src/main/java/com/example/programmingprogress/model/firebase/HistoryDataSource.kt
+++ app/src/main/java/com/example/programmingprogress/model/firebase/HistoryDataSource.kt
@@ -1,14 +1,15 @@
 package com.example.programmingprogress.model.firebase
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import java.sql.Timestamp
 import java.util.Date
 
 object HistoryDataSource {
     private val db = FirebaseFirestore.getInstance()
 
-    fun getQueryReviews(startDate: Timestamp, endDate: Timestamp) {
-        db.collection("users")
+    fun getQueryReviews(startDate: Timestamp, endDate: Timestamp): Query {
+        return db.collection("users")
             .document("tHKEs9c73mNfXcAkX1Mk").collection("history")
             .orderBy("date")
             .startAt(startDate)
