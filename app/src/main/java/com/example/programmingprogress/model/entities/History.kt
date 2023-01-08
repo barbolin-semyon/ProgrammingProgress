426ec0b0065ebcfb53f268a51b7befa881f50b6e
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-08 14:53:47 +0300

    Переименование поля isCheck

--- app/src/main/java/com/example/programmingprogress/model/entities/History.kt
+++ app/src/main/java/com/example/programmingprogress/model/entities/History.kt
@@ -1,11 +1,10 @@
 package com.example.programmingprogress.model.entities
 
-import java.sql.Timestamp
 import java.util.Date
 
 data class History(
     val date: Date = Date(),
     val description: String = "",
-    val isCheck: Boolean = false,
+    val check: Boolean = false,
     val hours: Double = 0.0,
 )
