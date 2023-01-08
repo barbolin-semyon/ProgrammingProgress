b5684b1f485821655dd21fc7bdca6fe9d4333310
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-08 14:14:36 +0300

    Добавлен параметр вращения

--- app/src/main/java/com/example/programmingprogress/ui/components/BackgroundCard.kt
+++ app/src/main/java/com/example/programmingprogress/ui/components/BackgroundCard.kt
@@ -7,13 +7,22 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.Dp
 import com.example.programmingprogress.ui.theme.Gray
 
 @Composable
-fun BackgroundCard(topPadding: Dp, angleRound: Dp, content: @Composable () -> Unit) {
+fun BackgroundCard(
+    topPadding: Dp,
+    angleRound: Dp,
+    rotation: Float = 0f,
+    content: @Composable () -> Unit
+) {
     Card(
-        modifier = Modifier.fillMaxSize().padding(top = topPadding),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = topPadding)
+            .graphicsLayer { rotationY = rotation },
         shape = RoundedCornerShape(topEnd = angleRound),
         backgroundColor = Gray
     ) {
