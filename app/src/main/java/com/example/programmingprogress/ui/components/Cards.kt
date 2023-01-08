a77e3263b5b6ffa5f7545094be7cb5820be2f20b
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-08 14:56:59 +0300

    Создание функции CardForHistory

--- app/src/main/java/com/example/programmingprogress/ui/components/Cards.kt
+++ app/src/main/java/com/example/programmingprogress/ui/components/Cards.kt
@@ -1,13 +1,22 @@
 package com.example.programmingprogress.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.programmingprogress.model.entities.History
+import com.example.programmingprogress.ui.theme.Blue
 import com.example.programmingprogress.ui.theme.Gray
+import com.example.programmingprogress.ui.theme.Red
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
 fun BackgroundCard(
@@ -25,5 +34,30 @@ fun BackgroundCard(
         backgroundColor = Gray
     ) {
         content()
+    }
+}
+
+@Composable
+fun CardForHistory(history: History, rotation: Float, onClick: () -> Unit) {
+    val format = SimpleDateFormat("dd MM yyyy")
+
+    Card(
+        modifier = Modifier
+            .padding(vertical = 4.dp)
+            .graphicsLayer(rotationY = rotation)
+            .clickable { onClick() },
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Row {
+            Text(
+                text = format.format(history.date),
+                modifier = Modifier.padding(16.dp)
+            )
+            Spacer(
+                modifier = Modifier
+                    .size(16.dp)
+                    .background(if (history.check) Blue else Red)
+            )
+        }
     }
 }
