90ad6985a487e4ad5c8589bb2d86cd1b0bae0836
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-22 19:00:49 +0300

    Добавление функции обновления истории для firebase

--- app/src/main/java/com/example/programmingprogress/model/firebase/HistoryDataSource.kt
+++ app/src/main/java/com/example/programmingprogress/model/firebase/HistoryDataSource.kt
@@ -1,5 +1,6 @@
 package com.example.programmingprogress.model.firebase
 
+import com.example.programmingprogress.model.entities.History
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import java.sql.Timestamp
@@ -7,6 +8,18 @@ import java.util.Date
 
 object HistoryDataSource {
     private val db = FirebaseFirestore.getInstance()
+
+    fun updateHistoryElement(
+        history: History,
+        onSuccess: () -> Unit,
+        onFailure: (message: Exception) -> Unit
+    ) {
+        db.collection("users")
+            .document("tHKEs9c73mNfXcAkX1Mk")
+            .collection("history").add(history)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailure(it) }
+    }
 
     fun getQueryReviews(startDate: Timestamp, endDate: Timestamp): Query {
         return db.collection("users")
