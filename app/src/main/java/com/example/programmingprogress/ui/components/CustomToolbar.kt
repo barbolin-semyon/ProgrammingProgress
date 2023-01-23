543ab50d1488b041245fe8f249ad8bdfca6386e0
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-23 23:15:05 +0300

    Добавление кнопки back

--- app/src/main/java/com/example/programmingprogress/ui/components/CustomToolbar.kt
+++ app/src/main/java/com/example/programmingprogress/ui/components/CustomToolbar.kt
@@ -6,21 +6,38 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.example.programmingprogress.R
 import com.example.programmingprogress.ui.theme.White
 
 @Composable
-fun CustomToolbar(title: String) {
-    Text(
-        text = title,
-        style = MaterialTheme.typography.h4,
+fun CustomToolbar(title: String, navHostController: NavHostController) {
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
-    )
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (navHostController.currentDestination!!.route != navHostController.graph.startDestinationRoute) {
+            IconButton(onClick = { navHostController.popBackStack() }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_back),
+                    contentDescription = "back",
+                    tint = White,
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+        }
+
+        Text(
+            text = title,
+            style = MaterialTheme.typography.h4,
+        )
+    }
 }
 
 @Composable
