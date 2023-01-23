e9fe7917e63e86b5bebf811d398403e99a2b7945
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-23 14:22:36 +0300

    Вынов функции getDate из History в отдельную Extension Function

--- app/src/main/java/com/example/programmingprogress/model/entities/History.kt
+++ app/src/main/java/com/example/programmingprogress/model/entities/History.kt
@@ -2,19 +2,14 @@ package com.example.programmingprogress.model.entities
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.sql.Timestamp
 import java.util.Calendar
 import java.util.Date
 
 @Parcelize
 data class History(
-    private val date: Date = Date(),
+    val date: Date = Date(),
     var description: String = "",
-    val check: Boolean = false,
+    var check: Boolean = false,
     var hours: Double = 0.0,
-) : Parcelable {
-    fun getDate(): Calendar {
-        return Calendar.getInstance().apply {
-            time = date
-        }
-    }
-}
+) : Parcelable
--- app/src/main/java/com/example/programmingprogress/util/ExtensionsFunction.kt
+++ app/src/main/java/com/example/programmingprogress/util/ExtensionsFunction.kt
@@ -3,9 +3,13 @@ package com.example.programmingprogress.util
 import java.text.SimpleDateFormat
 import java.util.*
 
+fun Date.parseCalendar(): Calendar {
+    val date = this
+    return Calendar.getInstance().apply { time = date }
+}
+fun Date.parseToShortString(): String = SimpleDateFormat("dd.MM.yyyy").format(this )
 fun Calendar.getDate() = this.get(Calendar.DATE)
 fun Calendar.getYear() = this.get(Calendar.YEAR)
 fun Calendar.getMonth() = this.get(Calendar.MONTH)
-fun Calendar.getDayOfWeek() = this.get(Calendar.DAY_OF_WEEK)
 
-fun Calendar.parseToShortString(): String = SimpleDateFormat("dd.MM.yyyy").format(this.time )
+fun Calendar.getDayOfWeek() = this.get(Calendar.DAY_OF_WEEK)
