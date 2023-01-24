0e87381055d177390dca79232417fa40ec31606a
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-24 11:53:38 +0300

    Добавление новой extension функции

--- app/src/main/java/com/example/programmingprogress/util/ExtensionsFunction.kt
+++ app/src/main/java/com/example/programmingprogress/util/ExtensionsFunction.kt
@@ -10,6 +10,7 @@ fun Date.parseCalendar(): Calendar {
 fun Date.parseToShortString(): String = SimpleDateFormat("dd.MM.yyyy").format(this )
 fun Calendar.getDate() = this.get(Calendar.DATE)
 fun Calendar.getYear() = this.get(Calendar.YEAR)
+fun Calendar.getDayOfYear() = this.get(Calendar.DAY_OF_YEAR)
 fun Calendar.getMonth() = this.get(Calendar.MONTH)
 
 fun Calendar.getDayOfWeek() = this.get(Calendar.DAY_OF_WEEK)
