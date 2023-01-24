1ed98f2fb2855b6ae0f70cc02324124120db34e1
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-25 02:21:44 +0300

    Реализация работы с авторизация и информацией пользователя

--- app/src/main/java/com/example/programmingprogress/model/firebase/UserDataSource.kt
+++ app/src/main/java/com/example/programmingprogress/model/firebase/UserDataSource.kt
@@ -1,9 +1,42 @@
 package com.example.programmingprogress.model.firebase
 
+import com.example.programmingprogress.model.entities.User
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object UserDataSource {
     private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
 
+    private val ioDispatcher = Dispatchers.IO
+    fun getUserId() = auth.currentUser?.uid
 
+    suspend fun signOut() = withContext(ioDispatcher) {
+        auth.signOut()
+    }
+
+    suspend fun signInWithEmail(email: String, password: String) = withContext(ioDispatcher) {
+        return@withContext auth.signInWithEmailAndPassword(email, password)
+    }
+
+    suspend fun createUser(email: String, password: String) = withContext(ioDispatcher) {
+        return@withContext auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    suspend fun updateUserScore(userId: String, data: Map<String, Object>) =
+        withContext(ioDispatcher) {
+            return@withContext db.collection("users").document(userId).update(data)
+        }
+
+    suspend fun addUserInDb(user: User) = withContext(ioDispatcher) {
+        return@withContext db.collection("users").document(user.id).set(user)
+    }
+
+    suspend fun getInformationUser() = withContext(ioDispatcher) {
+        return@withContext db.collection("users").document(getUserId()!!).get()
+    }
 }
--- app/src/main/java/com/example/programmingprogress/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/programmingprogress/viewmodel/UserViewModel.kt
@@ -1,10 +1,73 @@
 package com.example.programmingprogress.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.programmingprogress.model.entities.User
 import com.example.programmingprogress.model.firebase.HistoryDataSource
+import com.example.programmingprogress.model.firebase.UserDataSource
+import kotlinx.coroutines.launch
 
 class UserViewModel() : ViewModel() {
-    private val dataSource = HistoryDataSource
+    private val authDataSource = UserDataSource
 
+    private val _isRequestSuccess = MutableLiveData(false)
+    val isRequestSuccess: LiveData<Boolean>
+        get() = _isRequestSuccess
 
+    private val _isRequestError = MutableLiveData("")
+    val isRequestError: LiveData<String>
+        get() = _isRequestError
+
+    private val _userInfo by lazy { MutableLiveData(User()) }
+    val userInfo: LiveData<User>
+        get() = _userInfo
+
+    fun isAuthorization() = authDataSource.getUserId() != null
+
+    fun getInformationUser() = viewModelScope.launch {
+        if (authDataSource.getUserId() != null) {
+            authDataSource.getInformationUser()
+                .addOnSuccessListener {
+                    _userInfo.value = it.toObject(User::class.java)
+                }
+                .addOnFailureListener {
+                    _isRequestError.value = it.message
+                }
+        } else {
+            _isRequestError.value = "not authorization"
+        }
+    }
+    fun signInWithEmail(email: String, password: String) {
+        viewModelScope.launch {
+            authDataSource.signInWithEmail(email, password).addOnCompleteListener { task ->
+                with(task) {
+                    addOnSuccessListener {
+                        _isRequestSuccess.value = true
+                    }
+                    addOnFailureListener {
+                        _isRequestError.value = it.message
+                    }
+                }
+            }
+        }
+    }
+
+    fun signOut() = viewModelScope.launch {
+        authDataSource.signOut()
+    }
+
+    fun register(email: String, password: String) = viewModelScope.launch {
+        authDataSource.createUser(email, password).addOnCompleteListener { task ->
+            with(task) {
+                addOnSuccessListener {
+                    _isRequestSuccess.value = true
+                }
+                addOnFailureListener {
+                    _isRequestError.value = it.message
+                }
+            }
+        }
+    }
 }
