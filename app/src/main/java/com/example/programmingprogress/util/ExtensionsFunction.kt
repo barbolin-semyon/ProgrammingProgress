fa71689e1b06ecb408543605f6eadb4347ca652c
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-26 00:22:14 +0300

    Добавление функции валидности email

--- app/src/main/java/com/example/programmingprogress/util/ExtensionsFunction.kt
+++ app/src/main/java/com/example/programmingprogress/util/ExtensionsFunction.kt
@@ -1,5 +1,6 @@
 package com.example.programmingprogress.util
 
+import android.text.TextUtils
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -7,10 +8,15 @@ fun Date.parseCalendar(): Calendar {
     val date = this
     return Calendar.getInstance().apply { time = date }
 }
-fun Date.parseToShortString(): String = SimpleDateFormat("dd.MM.yyyy").format(this )
+
+fun Date.parseToShortString(): String = SimpleDateFormat("dd.MM.yyyy").format(this)
 fun Calendar.getDate() = this.get(Calendar.DATE)
 fun Calendar.getYear() = this.get(Calendar.YEAR)
 fun Calendar.getDayOfYear() = this.get(Calendar.DAY_OF_YEAR)
 fun Calendar.getMonth() = this.get(Calendar.MONTH)
-
 fun Calendar.getDayOfWeek() = this.get(Calendar.DAY_OF_WEEK)
+
+fun String.checkValidEmail() =
+    android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+
