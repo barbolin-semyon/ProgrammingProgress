603da9f85306f3d877722cc600a652f606b9bac1
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-26 14:25:22 +0300

    Изменение внешнего вида с использованем CustomTextField

--- app/src/main/java/com/example/programmingprogress/ui/screens/authorization/SignIn.kt
+++ app/src/main/java/com/example/programmingprogress/ui/screens/authorization/SignIn.kt
@@ -1,15 +1,17 @@
 package com.example.programmingprogress.ui.screens.authorization
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.programmingprogress.ui.components.CustomTextField
 import com.example.programmingprogress.ui.navigation.AuthorizationScreen
+import com.example.programmingprogress.ui.theme.Green
+import com.example.programmingprogress.ui.theme.White
 import com.example.programmingprogress.util.checkValidEmail
 import com.example.programmingprogress.viewmodel.AuthViewModel
 
@@ -20,28 +22,26 @@ fun SignInView(navHostController: NavHostController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
-    Column(Modifier.padding(8.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+    Column(
+        Modifier.padding(horizontal = 8.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        CustomTextField(
             value = email,
             onValueChange = { email = it },
             isError = email.checkValidEmail().not() && email.isNotEmpty(),
-            label = { Text(text = "Введите email") },
+            textLabel = "Введите email",
         )
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+        CustomTextField(
             value = password,
             onValueChange = { password = it },
             isError = password.length in 1..5,
-            label = {
-                Text(
-                    text = "Введите пароль " + if (password.length in 1..5)
-                        "(Меньше 6 символов)"
-                    else ""
-                )
-            },
+            textLabel = "Введите пароль " + if (password.length in 1..5)
+                "(Меньше 6 символов)"
+            else ""
         )
+
         Row(
             Modifier
                 .fillMaxWidth()
@@ -49,11 +49,16 @@ fun SignInView(navHostController: NavHostController) {
             horizontalArrangement = Arrangement.End
         ) {
             Button(
+                colors = ButtonDefaults.buttonColors(backgroundColor = Green),
                 modifier = Modifier.padding(end = 8.dp),
                 enabled = email.checkValidEmail() && password.length >= 6,
                 onClick = { authViewModel.signInWithEmail(email, password) }
             ) {
-                Text(text = "Войти", modifier = Modifier.padding(horizontal = 16.dp))
+                Text(
+                    text = "Войти",
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    color = White
+                )
             }
 
             Button(
