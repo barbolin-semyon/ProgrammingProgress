7e0dc3ecdf91e7f47e3c1539eac91c5e3363cc24
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-26 14:24:57 +0300

    Создание CustomTextField.kt

---
+++ app/src/main/java/com/example/programmingprogress/ui/components/CustomTextField.kt
@@ -0,0 +1,31 @@
+package com.example.programmingprogress.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.programmingprogress.ui.theme.Green
+
+@Composable
+fun CustomTextField(
+    textLabel: String,
+    value: String,
+    onValueChange: (value: String) -> Unit,
+    isError: Boolean
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Green,
+            focusedLabelColor = Green
+        ),
+        onValueChange = { changeValue(it) },
+        isError = isError,
+        label = {
+            Text(text = textLabel)
+        },
+    )
+}
