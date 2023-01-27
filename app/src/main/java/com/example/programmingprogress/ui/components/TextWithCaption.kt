1ee87ac997cb6a41409b065036258e3416a76771
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-27 21:25:48 +0300

    Изменение размера текста

--- app/src/main/java/com/example/programmingprogress/ui/components/TextWithCaption.kt
+++ app/src/main/java/com/example/programmingprogress/ui/components/TextWithCaption.kt
@@ -8,12 +8,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TextWithCaption(caption: String, text: String, modifier: Modifier = Modifier) {
     Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
         Text(text = caption, style = MaterialTheme.typography.h5)
-        Text(text = text, style = MaterialTheme.typography.body1, modifier = Modifier.padding(start = 8.dp))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Normal,
+            modifier = Modifier.padding(start = 8.dp)
+        )
     }
 }
