a40355e77a8280e3a5036d4ecfeb8e38d89b04eb
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-31 22:43:10 +0300

    Добавление Spinner

--- app/src/main/java/com/example/programmingprogress/ui/screens/checkpoint/Checkpoint.kt
+++ app/src/main/java/com/example/programmingprogress/ui/screens/checkpoint/Checkpoint.kt
@@ -6,8 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,6 +17,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.programmingprogress.ui.components.Item
+import com.example.programmingprogress.ui.components.Spinner
 import com.example.programmingprogress.ui.theme.*
 import com.example.programmingprogress.viewmodel.UserViewModel
 
@@ -30,92 +31,28 @@ fun Checkpoint(navHostController: NavHostController) {
         viewModel.getInformationUser()
     })
 
+    var currentCount by remember { mutableStateOf(0) }
+
     user.value?.let { user ->
-        val count = user.countOfDaysSuccess / 5
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Alpha)
-                .padding(top = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            items(count) { index ->
-                Item(
-                    number = (index + 1) * 5,
-                    backgroundColor = Green,
-                    textColor = White,
-                    colorSpacer = Green,
-                    isLast = (index + 1) == count
-                )
-            }
-
-            if (count == 0) {
-                item {
-                    Item(
-                        number = user.countOfDaysSuccess,
-                        backgroundColor = Green,
-                        textColor = White,
-                        colorSpacer = Green,
-                        isLast = true
-                    )
+        Column {
+            Spinner(
+                items = listOf(
+                    CheckpointCategory.HOURS,
+                    CheckpointCategory.CONSECUTIVE_DAYS,
+                    CheckpointCategory.ALL_DAY,
+                ),
+                hint = "Выберите тип",
+                padding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
+                onClick = { catecory ->
+                    currentCount = when (catecory) {
+                        CheckpointCategory.ALL_DAY -> user.countOfDaysSuccess
+                        CheckpointCategory.HOURS -> user.hours
+                        CheckpointCategory.CONSECUTIVE_DAYS -> user.countOfConsecutiveDaysSuccess
+                    }
                 }
-            } else {
-                item {
-                    Spacer(
-                        modifier = Modifier
-                            .width(10.dp)
-                            .height(100.dp)
-                            .background(brush = Brush.verticalGradient(listOf(Green, DarkGray)))
-                    )
-                }
-
-                items(2) { index ->
-                    Item(
-                        number = (index + 2) * count * 5,
-                        backgroundColor = Gray,
-                        textColor = DarkGray,
-                        colorSpacer = Color.DarkGray,
-                        isLast = index == 1
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun Item(
-    number: Int,
-    backgroundColor: Color,
-    textColor: Color,
-    colorSpacer: Color,
-    isLast: Boolean
-) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Card(
-            backgroundColor = backgroundColor,
-            shape = CircleShape,
-            elevation = 8.dp,
-            modifier = Modifier.size(100.dp),
-        ) {
-            Box(contentAlignment = Alignment.Center) {
-                Text(
-                    text = "$number",
-                    color = textColor,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 64.sp
-                )
-            }
-        }
-
-        if (isLast.not()) {
-            Spacer(
-                modifier = Modifier
-                    .width(10.dp)
-                    .height(100.dp)
-                    .background(color = colorSpacer)
             )
+
+            CheckpointViewForHoursOrDay(currentCount)
         }
     }
-
 }
