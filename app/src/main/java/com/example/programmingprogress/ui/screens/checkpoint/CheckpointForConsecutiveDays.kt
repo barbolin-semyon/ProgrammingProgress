4670abf77889a2b969acfc296968593613fb0657
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-01 01:49:45 +0300

    Реализация возможности обновления количества дней пользователя

--- app/src/main/java/com/example/programmingprogress/ui/screens/checkpoint/CheckpointForConsecutiveDays.kt
+++ app/src/main/java/com/example/programmingprogress/ui/screens/checkpoint/CheckpointForConsecutiveDays.kt
@@ -1,44 +1,74 @@
 package com.example.programmingprogress.ui.screens.checkpoint
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.programmingprogress.ui.theme.DarkGray
 import com.example.programmingprogress.ui.theme.Green
 import com.example.programmingprogress.ui.theme.Orange
+import com.example.programmingprogress.ui.theme.White
 import com.example.programmingprogress.viewmodel.HistoryViewModel
+import com.example.programmingprogress.viewmodel.UserViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CheckpointForConsecutiveDays(oldCount: Int) {
-    val viewModel = viewModel(HistoryViewModel::class.java)
-    val newCount = viewModel.countConsecutiveDays.observeAsState()
+    val historyViewModel = viewModel(HistoryViewModel::class.java)
+    val userViewModel = viewModel(UserViewModel::class.java)
+    val newCount = historyViewModel.countConsecutiveDays.observeAsState()
 
     LaunchedEffect(key1 = Unit, block = {
-        viewModel.getCountConsecutiveDays()
+        historyViewModel.getCountConsecutiveDays()
     })
 
     if (newCount.value != 0) {
-        LazyVerticalGrid(modifier = Modifier.fillMaxSize(), cells = GridCells.Adaptive(90.dp), content = {
-            items(oldCount) {
-                ItemDay(color = Orange)
-            }
+        Box(contentAlignment = Alignment.BottomCenter)   {
+            val razn = newCount.value!!.minus(oldCount)
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxSize().padding(bottom = 32.dp),
+                cells = GridCells.Adaptive(90.dp),
+                content = {
+                    items(oldCount) {
+                        ItemDay(color = Orange)
+                    }
 
-            items(newCount.value!!.minus(oldCount)) {
-                ItemDay(color = Green)
+                    items(razn) {
+                        ItemDay(color = Green)
+                    }
+                })
+
+            if (razn > 0) {
+                Button(
+                    onClick = { userViewModel.updateCountConsecutiveDays(newCount.value!!) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    colors = ButtonDefaults.buttonColors(Green)
+                ) {
+                    Text("Получить награду за побитый рекорд", color = White)
+                }
+                val state = userViewModel.isRequestSuccess.observeAsState()
+                if (state.value == true) {
+                    Text("Очки начислены", color = DarkGray)
+                }
             }
-        })
+        }
     }
+
+
 }
 
 @Composable
